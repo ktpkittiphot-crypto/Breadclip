7ff779e20ae4a796f02fe143417734d9e8cf6ade
ktpkittiphot-crypto <ktpkittiphot@gmail.com>
2026-07-25 04:58:28 +0700

    Load custom Bread Clip logo styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './logo.css'
 import App from './App.jsx'
 
 const BACKEND_URL = 'https://script.google.com/macros/s/AKfycbyJSHTGFeJOQVoMGk5lxEblPyJ080L3dWKlJ5rhQN-2vprbSF_RWQ2gOKYMG_KiATSq/exec'
