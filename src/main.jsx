0981a484b8e105ae0768d73d9d18272d6e84b585
ktpkittiphot-crypto <ktpkittiphot@gmail.com>
2026-07-25 05:12:17 +0700

    Hide hero bio from storefront

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './logo.css'
+import './hide-hero-bio.css'
 import './admin-guard.js'
 import App from './App.jsx'
 
