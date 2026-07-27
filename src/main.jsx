00617779b10d8e848b5aeb744fc4ec7a4729f273
ktpkittiphot-crypto <ktpkittiphot@gmail.com>
2026-07-27 15:45:10 +0700

    Load coupon minimum rule

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './logo.css'
 import './hide-hero-bio.css'
 import './admin-guard.js'
 import './promptpay-lock.js'
+import './coupon-minimum-rule.js'
 
 const BACKEND_URL = 'https://script.google.com/macros/s/AKfycbw4DwcxpK_EBxfaLezs1q37j7gay2tLpamiPZzYobW8YeYrV79b5JQ_OFJENR-nOMmH/exec'
 const PROMPTPAY_ID = '1679900640970'
